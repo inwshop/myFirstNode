dd7d8d3307d0a54220c34df526598e095d90dcaf
inwshop <pakhapong.swu@g.swu.ac.th>
2017-09-23 12:13:46 +0700

    2

--- config/express.js
+++ config/express.js
@@ -3,6 +3,9 @@ import morgan from 'morgan';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 
+import session from 'express-session';
+import passport from 'passport';
+
 module.exports = function () {
     const app = express();
 
@@ -18,6 +21,15 @@ module.exports = function () {
     app.use(bodyParser.urlencoded({
         extended: true
     }));
+
+    var config = require('./config');
+    app.use(session({
+        secret: config.sessionSecret,
+        resave: false,
+        saveUninitializeed: true
+    }));
+    app.use(passport.initialize()); // start passport
+    app.use(passport.session()); // use session via express-session
 
     require('../app/routes/index.route')(app);
     require('../app/routes/user.route')(app);
