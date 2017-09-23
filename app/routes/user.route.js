3c33a169af26e24930fbdb67434835c0afb8872a
inwshop <pakhapong.swu@g.swu.ac.th>
2017-09-23 11:39:54 +0700

    2

---
+++ app/routes/user.route.js
@@ -0,0 +1,6 @@
+var user = require('../controllers/user.controller');
+module.exports = (app) => {
+    var path = '/api/user';
+    app.get(path + '/getuser', user.getUsers);
+    app.post(path + '/signup', user.create);
+}
--- config/express.js
+++ config/express.js
@@ -3,18 +3,23 @@ import morgan from 'morgan';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 
-module.exports = function() {
-const app = express();
+module.exports = function () {
+    const app = express();
 
-if (process.env.NODE_ENV === "development") {
-app.use(morgan('dev'));
-}
-else {
-app.use(compression());
-}
+    if (process.env.NODE_ENV === "development") {
+        app.use(morgan('dev'));
+    }
+    else {
+        app.use(compression());
+    }
 
-app.use(bodyParser.json());
-require('../app/routes/index.route')(app);
-return app;
+    app.use(bodyParser.json());
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    require('../app/routes/index.route')(app);
+    require('../app/routes/user.route')(app);
+    return app;
 }
-show dbs 
